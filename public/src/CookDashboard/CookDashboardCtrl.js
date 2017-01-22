e618fe2c70c21f8302083ac3fdb658a3c49fbf33
adaptedbee <adaptedbee@yandex.ru>
2017-01-22 05:47:26 +0300

    Убираем вывод сообщений в консоль

--- public/src/CookDashboard/CookDashboardCtrl.js
+++ public/src/CookDashboard/CookDashboardCtrl.js
@@ -4,32 +4,24 @@ droneCafeApp.controller('CookDashboardCtrl', function($scope, CookDashboardServi
 
   CookDashboardService.getDishes('Ordered').then(function(data) {
       $scope.orderedDishes = data.data;
-  }, function(error) {
-      console.log('Error: ' + error);
   });
 
   CookDashboardService.getDishes('Cooking').then(function(data) {
       $scope.cookingDishes = data.data;
-  }, function(error) {
-      console.log('Error: ' + error);
   });
 
 
   $scope.startCooking = function(orderid){
-    console.log(orderid);
-
     CookDashboardService.updateOrderStatus(orderid, 'Cooking').then(function(data) {
-        console.log(data.data);
+        // console.log(data.data);
 
         // update orders lists
     });
   };
 
   $scope.finishCooking = function(orderid){
-    console.log(orderid);
-
     CookDashboardService.updateOrderStatus(orderid, 'In delivery').then(function(data) {
-        console.log(data.data);
+        // console.log(data.data);
 
         // update orders lists
     });
